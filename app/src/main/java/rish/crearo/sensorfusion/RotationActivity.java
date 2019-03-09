9f9cad26687421f2afb84f8515f90081d171b0aa
crearo <rishabh.bhardwaj@tonboimaging.com>
2019-03-09 20:45:19 +0530

    Add an attitude indicator and update gradle

--- app/src/main/java/rish/crearo/sensorfusion/RotationActivity.java
+++ app/src/main/java/rish/crearo/sensorfusion/RotationActivity.java
@@ -1,7 +1,7 @@
 package rish.crearo.sensorfusion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class RotationActivity extends AppCompatActivity {
 
